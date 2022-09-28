4ad3d184312c44df0e0bcb75fc82dbb0771ec767
hankangwen <13538898378@163.com>
2022-09-28 14:15:52 +0800

    Create repository.

---
+++ CSharpClient/CSharpClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpClient
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+        }
+    }
+}
